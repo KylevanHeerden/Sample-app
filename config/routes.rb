8d348074cbb24dab1212bb6dc0da2097dc5464c3
Kyle <kylevanheerden@ymail.com>
2020-02-24 16:59:43 +0200

    finishing signup but error at form_for

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  resources :user
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
